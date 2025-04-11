b18589b3ba0ea1203849b9dc1a250fe2765bc0f6
atina <atina@10.18.54.26>
2025-04-12 01:13:15 +0300

    new field of study created

---
+++ csjsimscore/src/domain/FieldOfStudy.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author atina
+ */
+public class FieldOfStudy {
+      int id;
+      String  name;
+      int departementid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDepartementid() {
+        return departementid;
+    }
+
+    public void setDepartementid(int departementid) {
+        this.departementid = departementid;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
